fc0c8bb740e7430ee572079a62938cb985111ca4
Raphael Baron <raphaelbaron@gmail.com>
2013-07-11 00:48:53 -0300

    - term_clear is working;
    - A clear term with a pretty header is created upon its call.
    
    	modified:   asm/term.asm

--- asm/term.asm
+++ asm/term.asm
@@ -1,7 +1,13 @@
+; asm/term.asm
+;
+; Handles output.
+
+
 [bits 32]
 
-TERM_WIDTH: equ 1
-TERM_HEIGHT: equ 8
+TERM_WIDTH: equ 80 
+TERM_HEIGHT: equ 25
+TERM_LENGTH: equ TERM_WIDTH*TERM_HEIGHT
 
 
 global term_clear
@@ -10,8 +16,12 @@ global term_write
 
 section .data
 
+term_header_bg_color: db 0x2
+term_header_fg_color: db 0x1
 term_bg_color: db 0x5
 term_fg_color: db 0x3
+
+term_header_text: db 's','o','s'
 
 section .bss
 
@@ -24,22 +34,85 @@ term_clear:
   mov ebx, 0
   mov eax, 0xB8000
 
-  loop:
-    mov ebx, [term_bg_color]
-    mov ecx, [term_fg_color]
-    shl ecx, 4
-    or ebx, ecx
-    shl ebx, 8
+  mov ebx, [term_header_bg_color]
+  mov ecx, [term_header_fg_color]
 
-    ; Space char (ASCII 0x20)
-    or ebx, 0x22
+  ; Set color and space char for cleanup
+  shl ecx, 4
+  or ebx, ecx
+  shl ebx, 8
+  ; % char  (ASCII 0x25)
+  or ebx, 0x25
 
-    mov [eax+edx], ebx
+  header_left:
 
+    mov [eax+2*edx], ebx
     inc edx 
-    ;cmp ax, TERM_WIDTH*TERM_HEIGHT
-    cmp edx, 0x1
-    jl loop
+    cmp edx, TERM_WIDTH/2-4
+    jl header_left
+
+  header_middle:
+    and ebx, 0xFFFFFF00
+    ; ASCII [
+    or ebx, 0x5b
+    mov [eax+2*edx], ebx
+    inc edx 
+
+    and ebx, 0xFFFFFF00
+    ; ASCII s
+    or ebx, 0x73
+    mov [eax+2*edx], ebx
+    inc edx 
+
+    and ebx, 0xFFFFFF00
+    ; ASCII o
+    or ebx, 0x6f
+    mov [eax+2*edx], ebx
+    inc edx 
+
+    and ebx, 0xFFFFFF00
+    ; ASCII s
+    or ebx, 0x73
+    mov [eax+2*edx], ebx
+    inc edx 
+
+    and ebx, 0xFFFFFF00
+    ; ASCII ]
+    or ebx, 0x5d
+    mov [eax+2*edx], ebx
+    inc edx 
+
+  and ebx, 0xFFFFFF00
+  ; ASCII %
+  or ebx, 0x25
+  mov [eax+2*edx], ebx
+  inc edx 
+
+  header_bottom:
+
+    mov [eax+2*edx], ebx
+    inc edx 
+    cmp edx, TERM_WIDTH
+    jl header_bottom
+    
+
+  mov ebx, [term_bg_color]
+  mov ecx, [term_fg_color]
+
+  ; Set color and space char for cleanup
+  shl ecx, 4
+  or ebx, ecx
+  shl ebx, 8
+  ; Space char (ASCII 0x20)
+  or ebx, 0x20
+
+  body:
+
+    mov [eax+2*edx], ebx
+    inc edx 
+    cmp edx, TERM_LENGTH
+    jl body 
 
   popad
   ret
+
